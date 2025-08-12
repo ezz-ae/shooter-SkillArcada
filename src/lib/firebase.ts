a24f8cc6ebe573f453eb2dd8dd44f30c4b5e5b80
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-12 09:32:57 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAnalytics, isSupported } from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  projectId: "shopnluck",
+  appId: "1:841616141451:web:dc8b5c582b07f907e3b88e",
+  storageBucket: "shopnluck.firebasestorage.app",
+  apiKey: "AIzaSyBavgW2z5rCdZAEeBdoA5wQ53E7qvHFc7o",
+  authDomain: "shopnluck.firebaseapp.com",
+  messagingSenderId: "841616141451"
+};
+
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Initialize Firebase Analytics
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, analytics };
