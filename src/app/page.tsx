b9646fd7506a0983eb289320cd359308660e66fc
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-12 23:05:53 +0000

    i'm still not happy with the home page i loved the diece and it was much

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 
 "use client";
 
-import { LuckSession } from "@/components/luck-session";
+import { DiceGame } from "@/components/dice-game";
 
 export default function Home() {
     return (
         <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center min-h-[calc(100vh-8rem)]">
-            <LuckSession />
+            <DiceGame />
         </div>
     );
 }
