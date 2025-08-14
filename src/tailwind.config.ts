ae6e0ec63a64e12c45a8743749f9fb84489b045c
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-14 09:10:52 +0000

    i need the app

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,5 +1,4 @@
-
-import type {Config} from 'tailwindcss';
+import type { Config } from 'tailwindcss';
 
 const config = {
   darkMode: ['class'],
@@ -69,14 +68,14 @@ const config = {
       },
       keyframes: {
         'accordion-down': {
-          from: {height: '0'},
-          to: {height: 'var(--radix-accordion-content-height)'},
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
-          from: {height: 'var(--radix-accordion-content-height)'},
-          to: {height: '0'},
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
-         'border-pulse': {
+        'border-pulse': {
           '0%, 100%': { 'border-color': 'hsl(var(--accent) / 0.2)' },
           '50%': { 'border-color': 'hsl(var(--accent) / 0.8)' },
         },
