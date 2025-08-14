ddc0f2c3178ec93326a1227a9dde90e2952af001
Mahmoud Ezz <Mu7moud@gmail.com>
2025-08-14 11:11:22 +0000

    thome home page backgroound is white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,12 @@ export default function Home() {
   
   return (
     <>
-      <div className="w-full flex items-center justify-center min-h-[calc(100vh-4rem)] bg-background">
+      <div className="w-full flex items-center justify-center min-h-[calc(100vh-4rem)]">
           <LuckSession />
       </div>
-      <div className="py-12 bg-background">
+      <div className="py-12">
         <FeaturedContent />
       </div>
     </>
   );
 }
-
